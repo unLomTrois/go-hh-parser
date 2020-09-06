bdbc353a135eab35044867f72ebf2d9c5945c3c2
unlomtrois <vadim.kuz02@gmail.com>
2020-09-06 09:37:55 +0300

    add sync loop

--- cmd/go-hh-parser/main.go
+++ cmd/go-hh-parser/main.go
@@ -8,11 +8,19 @@ import (
 func main() {
 	core := core.New()
 
-	vacancy, err := core.Req.GetVacancy("https://api.hh.ru/vacancies/38468170")
-	if err != nil {
-		log.Fatal(err)
+	urls := []string{
+		"https://api.hh.ru/vacancies/38468170",
+		"https://api.hh.ru/vacancies/38468172",
+		"https://api.hh.ru/vacancies/38468173",
 	}
 
-	log.Println(vacancy.Data)
+	for _, url := range urls {
 
+		vacancy, err := core.Req.GetVacancy(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(vacancy.Data)
+	}
 }
